c342db355b2edf83f604c630957578bcc539d224
Tim <timellis337@gmail.com>
2024-12-17 15:48:13 -0800

    reimplement setPercentOut methods

--- src/main/java/frc/robot/commands/endEffector/EndEffectorPercentOut.java
+++ src/main/java/frc/robot/commands/endEffector/EndEffectorPercentOut.java
@@ -25,13 +25,13 @@ public class EndEffectorPercentOut extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // endEffector.setVoltage(2);
+    endEffector.setPercentOut(0.2);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // endEffector.setVoltage(0);
+    endEffector.setPercentOut(0);
   }
 
   // Returns true when the command should end.
