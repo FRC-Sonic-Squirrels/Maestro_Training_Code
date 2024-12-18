c4ed61fdfb9f8afafccd532cde24e1bef016f632
Tim <timellis337@gmail.com>
2024-12-17 16:10:21 -0800

    added second tof pt 2

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIO.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIO.java
@@ -3,11 +3,15 @@ package frc.robot.subsystems.endEffector;
 public interface EndEffectorIO {
   /** Contains all of the input data received from hardware. */
   public static class Inputs {
+    // Motor
     public double deviceTemp;
     public double appliedVolts;
     public double currentAmps;
     public double velocityRPM;
+
+    // Time of Flights
     public double intakeSideTOFDistanceInches;
+    public double shooterSideTOFDistanceInches;
   }
 
   public default void updateInputs(Inputs inputs) {}
